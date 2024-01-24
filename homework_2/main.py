d9b0c62136f0a9af67fa2737878bae73394cc1d0
konsgrib <konstantin@gribanov.net>
2024-01-24 22:41:28 +0000

    homework2 classes are added

---
+++ homework_2/main.py
@@ -0,0 +1,53 @@
+from abc import ABC
+import math
+
+
+class Mover:
+    def __init__(self, speed):
+        self.speed = speed
+
+    def move(self, position, velocity, distance):
+        angle = math.atan2(velocity[1], velocity[0])
+        new_position = (
+            position[0] + distance * self.speed * math.cos(angle),
+            position[1] + distance * self.speed * math.sin(angle),
+        )
+
+        return new_position
+
+
+class Rotator:
+    def __init__(self, rotation):
+        self.rotation = rotation
+
+    def rotate(self, angle):
+        self.rotation = (self.rotation + angle) % 360
+
+        return self.rotation
+
+
+class FlyingObject(ABC):
+    def __init__(self, position, velocity, mover, rotator):
+        self.position = position
+        self.velocity = velocity
+        self.mover = mover
+        self.rotator = rotator
+
+    def move_forward(self, distance):
+        self.position = self.mover.move(self.position, self.velocity, distance)
+
+    def rotate(self, angle):
+        self.rotation = self.rotator.rotate(angle)
+
+    def destroy(self):
+        self.status = "Destroyed"
+
+
+class SpaceShip(FlyingObject):
+    def fire_torpedo(self):
+        torpedo = Torpedo(self.position, self.velocity, self.mover, self.rotator)
+        return torpedo
+
+
+class Torpedo(FlyingObject):
+    pass
---
+++ homework_2/test_main.py
@@ -0,0 +1,97 @@
+import pytest
+import math
+from main import Mover, Rotator, FlyingObject, SpaceShip, Torpedo
+
+
+def test_mover():
+    mover = Mover(1)
+
+    assert mover.move((0, 0), (1, 0), 1) == (1, 0)
+
+
+def test_rotator():
+    rotator = Rotator(0)
+
+    assert rotator.rotate(90) == 90
+
+
+def test_flying_object():
+    mover = Mover(1)
+    rotator = Rotator(0)
+    flying_object = FlyingObject((0, 0), (1, 0), mover, rotator)
+    flying_object.move_forward(1)
+    assert flying_object.position == (1, 0)
+    flying_object.rotate(90)
+    assert flying_object.rotation == 90
+    flying_object.destroy()
+    assert flying_object.status == "Destroyed"
+
+
+def test_spaceship():
+    mover = Mover(1)
+    rotator = Rotator(0)
+    spaceship = SpaceShip((0, 0), (1, 0), mover, rotator)
+    torpedo = spaceship.fire_torpedo()
+    assert isinstance(torpedo, Torpedo)
+
+
+def test_torpedo():
+    mover = Mover(1)
+    rotator = Rotator(0)
+    torpedo = Torpedo((0, 0), (1, 0), mover, rotator)
+    assert isinstance(torpedo, Torpedo)
+
+
+def test_mover_move():
+    mover = Mover(1)
+    position = (12, 5)
+    velocity = (-7, 3)
+    distance = 1
+    angle = math.atan2(velocity[1], velocity[0])
+    expected_new_position = (
+        position[0] + distance * mover.speed * math.cos(angle),
+        position[1] + distance * mover.speed * math.sin(angle),
+    )
+    actual_new_position = mover.move(position, velocity, distance)
+    
+    assert actual_new_position == expected_new_position
+
+
+def test_mover_move_no_position():
+    mover = Mover(1)
+
+    class ObjectWithoutPosition:
+        velocity = (-7, 3)
+
+    obj = ObjectWithoutPosition()
+    with pytest.raises(AttributeError):
+        mover.move(obj.position, obj.velocity, 1)
+
+
+def test_mover_move_no_velocity():
+    mover = Mover(1)
+
+    class ObjectWithoutVelocity:
+        position = (12, 5)
+
+    obj = ObjectWithoutVelocity()
+
+    with pytest.raises(AttributeError):
+        mover.move(obj.position, obj.velocity, 1)
+
+
+def test_mover_move_no_position_setter():
+    mover = Mover(1)
+
+    class ObjectWithoutPositionSetter:
+        position = (12, 5)
+        velocity = (-7, 3)
+
+        @property
+        def position(self):
+            return self._position
+
+    obj = ObjectWithoutPositionSetter()
+
+    with pytest.raises(AttributeError):
+        mover.move(obj.position, obj.velocity, 1)
